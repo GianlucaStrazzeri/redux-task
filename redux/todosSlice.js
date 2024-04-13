3af8a6822c96d62d4b2f1fd1aaed6d10c8c76b3b
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2024-04-13 09:45:42 +0200

    more changes

--- redux/todosSlice.js
+++ redux/todosSlice.js
@@ -1,1 +1,16 @@
 import {createSlice} from "@reduxjs/toolkit";
+
+const initialState ={
+    taskName: null,
+    taskDescription: null,
+    completed:false
+}
+
+export const taskSlice = createSlice({
+name:"task",
+initialState,
+reducers: {
+addTask: {state,action}
+}
+
+})
