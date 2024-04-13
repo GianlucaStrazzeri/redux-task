55b601eedacd2207f88ba3cc28a586679ad69867
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2024-04-13 09:36:59 +0200

    first changes

--- redux/store.js
+++ redux/store.js
@@ -1,1 +1,17 @@
-function reducer(){};
+//Nos dirá cuales son los estados con los que iremos a trabajar
+
+import { configureStore } from "@reduxjs/toolkit"; //importo configurestore desde el toolkit de redux
+import useReducer from "./todosSlice"
+
+
+
+
+
+
+//Los reducer son los estados que vamos a estar controlando que dividiremos en porciones (slice)
+//todas las configuaraciones o la gran mayoría de ellas van en un objeto
+export const store = configureStore(
+    {
+    reducer: userReducer
+}
+)
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {Provider} from "react-redux" //Esto sirve para importar el provider de redux, 
+import store from "./redux.store.js" //importo store desde la carpeta redux
 
-
+//Provider envolverá toda la aplicación para que el estado lo podamos utilizar en toda la aplicación
+//El provider tiene un atributo el store que es donde guardaremos el estado (el store es inmutable) 
+//Aquí no utilizaremos el useState ya que redux es un manejador de estados
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <App />
+    <Provider store={store}> 
+    <App />
+    </Provider>
+      
   </React.StrictMode>,
 )
